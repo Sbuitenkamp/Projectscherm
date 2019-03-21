629695d365698c9a6a5b4c331c2730283ea146e9
Steven Buitenkamp <developer.buitenkamp@gmail.com>
2019-03-21 09:56:35 +0100

    started with POST/GET

--- index.js
+++ index.js
@@ -3,19 +3,11 @@ const fs = require('fs');
 const url = require('url');
 const db = require('./init_files/db')();
 
-
 function onRequest(request, response) {
     let path = url.parse(request.url).pathname;
     response.writeHead(200, { 'Content-Type': 'text/html' });
-    if (path === '/') {
-        fs.readFile('./views/index.html', null, (err, data) => {
-            if (err) {
-                response.writeHead(404, { 'Content-Type': 'text/html' });
-                response.write('Page not found');
-            } else response.write(data);
-            response.end();
-        });
-    } else {
+    if (path === '/') loadFile(path, response);
+    else {
         if (path.includes('.')) {
             path = (() => {
                 const parts = path.split(/\.+/g);
@@ -23,14 +15,25 @@ function onRequest(request, response) {
                 return parts.join('.');
             })();
         }
-        fs.readFile(`./views${path}.html`, null, (err, data) => {
-            if (err) {
-                response.writeHead(404, { 'Content-Type': 'text/html' });
-                response.write('Page not found');
-            } else response.write(data);
-            response.end();
-        });
+        loadFile(path, response);
     }
+}
+
+function loadFile(path, response) {
+    fs.readFile(`./views${path}.html`, null, (err, data) => {
+        if (err) {
+            response.writeHead(404, { 'Content-Type': 'text/html' });
+            response.write('Page not found');
+        } else response.write(data);
+        // TODO create post request
+        // http.request({
+        //     host: 'localhost',
+        //     path: '/index',
+        //     method: 'POST',
+        //
+        // });
+        response.end();
+    });
 }
 
 http.createServer(onRequest).listen(4200, () => console.log('Listening on port 4200'));

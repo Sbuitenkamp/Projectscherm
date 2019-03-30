afe9f45672fb50f49570508cba646b99aebe5177
Steven Buitenkamp <developer.buitenkamp@gmail.com>
2019-03-30 13:58:13 +0100

    adding project finished, some minor tweaks to server, new node package added for dates

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
+const moment = require('moment');
 const db = require('./models/db')();
 const app = express();
 const port = process.env.PORT || 4200;
@@ -28,47 +29,38 @@ app.get('/:path', (req, res) => {
     else res.send('404 Page not found');
 });
 
-app.post('/create', async (req, res) => {
-    const data = await create(req.body.table, req.body.values);
-    res.send(data);
-});
+app.post('/create', async (req, res) => res.send(await create(req.body.table, req.body.options)));
+app.post('/select', async (req, res) => res.send(await select(req.body.table, req.body.options)));
+app.post('/update', async (req, res) => res.send(await update(req.body.table, req.body.values, req.body.options)));
+app.post('/destroy', async (req, res) => res.send(await destroy(req.body.table, req.body.options)));
 
-app.post('/select', async (req, res) => {
-    console.log(req.body.options);
-    const data = await select(req.body.table, req.body.options);
-    res.send(data);
+// use to format dd-mm-yyyy
+app.post('/moment', (req, res) => {
+    const dates = {};
+    for (const date in req.body.dates) dates[date] = moment(req.body.dates[date], 'dd-mm-yyyy').format('x');
+    res.send(dates);
 });
-
-app.post('/update', async (req, res) => {
-    const data = await update(req.body.table, req.body.values, req.body.options);
-    res.send(data);
-});
-
-app.post('/destroy', async (req, res) => {
-    const data = await destroy(req.body.table, req.body.options);
-    res.send(data);
-});
-
 
 // query functions
 async function create(table, values) {
     if (!table) return;
     if (!values) return;
-    return await db[table].create(values).then(res => res);
+    console.log('yes')
+    return await db[table].create(values).catch(e => console.error(e));
 }
 
 async function select(table, options) {
     if (!table) return;
     if (!options) options = {};
-    if (options.limit === 1) return await db[table].findOne(options).then(res => res);
-    else return await db[table].findAll(options).then(res => res);
+    if (options.limit === 1) return await db[table].findOne(options);
+    else return await db[table].findAll(options);
 }
 
 async function update(table, values, options) {
     if (!table) return;
     if (!values) return;
     if (!options) options = {};
-    return await db[table].update(values, options).then(res => res);
+    return await db[table].update(values, options);
 }
 
 async function destroy(table, options) {

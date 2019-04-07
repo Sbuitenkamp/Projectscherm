9e5b94751abb10818c28379469fa2a86261677bb
sbuitenkamp <buitenkamp.developer@gmail.com>
2019-04-07 23:28:54 +0200

    unhardcoded managerid

--- controllers/manager-add-team.js
+++ controllers/manager-add-team.js
@@ -1,10 +1,10 @@
 async function submit() {
     const form = document.getElementById('add-team-form');
     const formData = {
-        managerId: 1,
         username: form.username.value,
         members: form.members.value
     };
+    await $.post('/send-session', null, session => formData.managerId = session.id);
     await $.post('/hash', { password: form.password.value }, hashedPassword => formData.password = hashedPassword);
     for (const key in formData) {
         if (key === 'members') continue;

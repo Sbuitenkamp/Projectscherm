e747183e1e33eb60cd27c720621be40211d8bb39
Steven Buitenkamp <developer.buitenkamp@gmail.com>
2019-04-09 15:46:06 +0200

    project assigning added, database relations added

--- controllers/generalFunctions.js
+++ controllers/generalFunctions.js
@@ -6,28 +6,26 @@ function parseDates(table, col) {
             let month = String(date.getMonth() + 1);
             let day = String(date.getDate());
             const year = String(date.getFullYear());
-            if (day.length < 2) {
-                day = '0' + day;
-            }
-            if (month.length < 2) {
-                month = '0' + month;
-            }
+            if (day.length < 2) day = '0' + day;
+            if (month.length < 2) month = '0' + month;
             return `${day}-${month}-${year}`;
         })();
     }
     if (col === 'delay') table[col] = table[col] ? `+${table[col]} ${table[col] === 1 ? ' dag' : 'dagen'}` : 'geen';
 }
 
-function renderSelectData({ data, tableId, deleteBtn }) {
+function renderSelectData({ data, tableId, deleteBtn, editBtn }) {
     data.forEach((result) => {
         const index = result.id;
         document.getElementById(tableId).innerHTML += `<tr id='result${index}'></tr>`;
         for (const col in result) {
             if (col.trim().toLowerCase() === 'id') continue;
             parseDates(result, col);
-            document.getElementById(`result${index}`).innerHTML += `<td>${result[col] || 'geen'}</td>`
+            console.log(result);
+            document.getElementById(`result${index}`).innerHTML += `<td>${result[col] ? result[col].username || result[col] : 'geen'}</td>`
         }
-        if (deleteBtn) document.getElementById(`result${index}`).innerHTML += `<td><button onclick='remove(${index});'>Verwijderen</button></td>`
+        if (deleteBtn) document.getElementById(`result${index}`).innerHTML += `<td><button onclick="remove(${index});">Verwijderen</button></td>`;
+        if (editBtn) document.getElementById(`result${index}`).innerHTML += `<td><button onclick="editProj(${index});">Project aanpassen</button></td>`;
     });
 }
 
---
+++ controllers/manager-edit-project.js
@@ -0,0 +1,33 @@
+let id;
+window.onload = () => {
+    const dropdown = document.getElementById('teamlist');
+    $.post('/send-session', null, session => {
+        id = session.savedId;
+        $.post('/select', {
+            table: 'teams',
+            options: {
+                attributes: [
+                    'id',
+                    'username'
+                ],
+                where: { managerId: session.id },
+            }
+        }, data => {
+            for (const team of data) dropdown.innerHTML += `<option value="${team.id}">${team.username}</option>`;
+        })
+    });
+
+};
+
+function submit() {
+    const dropdown = document.getElementById('teamlist');
+    console.log(dropdown.value);
+    $.post('/update', {
+        table: 'projects',
+        values: { teamId: dropdown.value },
+        options: { where: { id } }
+    }, result => {
+        if (!result) alert('Er is iets misgegaan met het updaten van het project');
+        else window.location = '/manager-projects';
+    });
+}
